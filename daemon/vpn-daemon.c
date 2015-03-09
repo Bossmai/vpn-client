5bd80da73fba15029411eac2a7fcf825351e8c2d
eric.chan <chenyijin1983@gmail.com>
2015-03-09 21:59:50 +0800

    Init commit

---
+++ daemon/vpn-daemon.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(int argc, char *argv[])
+{
+	//printf("vpn-daemon start to run...\n");
+	while(1)
+	{		
+		//printf("vpn-daemon is running...\n");
+		sleep(10);
+	}
+	//printf("vpn-daemon Bye!\n");
+	return 0;
+}
